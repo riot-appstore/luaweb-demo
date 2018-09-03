6c6911164346d3c57f67e44357b87b88b33377a1
Juan I Carrano <juan@carrano.com.ar>
2018-09-03 19:07:00 +0200

    Simple hacky rolling time series

--- chart-test.js
+++ chart-test.js
@@ -1,25 +1,41 @@
 // based on prepared DOM, initialize echarts instance
 var myChart = echarts.init(document.getElementById('graphs'));
 
+
+var randomsequence = Array.from({length: 25}, () => Math.random());
+
 // specify chart configuration item and data
 var option = {
     title: {
-	text: 'ECharts entry example'
+	text: 'Random noise'
     },
     tooltip: {},
     legend: {
 	data:['Sales']
     },
-    xAxis: {
-	data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-    },
     yAxis: {},
+    xAxis: {data:Array.from(Array(25).keys())},
     series: [{
-	name: 'Sales',
-	type: 'bar',
-	data: [5, 20, 36, 10, 10, 20]
+	name: 'Noise',
+	type: 'line',
+	data: randomsequence
     }]
 };
 
 // use configuration item and data specified to show chart
 myChart.setOption(option);
+
+var k = 0;
+
+window.setInterval(function(){
+    k = k+1;
+    randomsequence.shift();
+    randomsequence.push(Math.random());
+    myChart.setOption({
+	xAxis: {data:Array.from(Array(25).keys()).map((i) => i+k)},
+	series: [{
+	    name: 'Noise',
+	    type: 'line',
+	    data: randomsequence
+	}]});
+}, 500);
