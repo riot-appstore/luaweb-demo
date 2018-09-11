88634867c92d1728a86bc4f777a0b6ddc85a232a
Juan I Carrano <juan@carrano.com.ar>
2018-09-11 15:38:38 +0200

    Graphics and simulation working 100%

--- chart-test.js
+++ chart-test.js
@@ -5,18 +5,19 @@ var system = {
 		width: 0.3,
 		distance: 1,
 		intensity: 1.0,
-		bg_intensity: 0.2,
-		bg_noise: 0.05,
-		fg_noise: 0.05
+		bg_intensity: 0.1,
+		bg_noise: 0.03,
+		fg_noise: 0.03
 	    },
-	sensor_noise: 0.05,
-	servo_speed: 1,
+	sensor_noise: 0.01,
+	servo_speed: 0.001,
 	sample_rate: 100,
-	plot_divisor: 20
+	plot_divisor: 10,
+	sensor_gain: 450
     },
     state: {
 	orientation: 0.0,
-	setpoint: 0.0,
+	setpoint: 1500.0,
 	sensor_value: 0.0,
 	_light_noise: 0.0
     }
@@ -37,6 +38,11 @@ function angle_wrap(a)
 function urand(a)
 {
     return a * (Math.random() - 0.5);
+}
+
+function setpoint2orientation(sp)
+{
+    return ((sp-1500)/1000) * Math.PI;
 }
 
 /* simple mid-point numerical integration */
@@ -80,15 +86,15 @@ function incident_light(system, angle)
 
 function update_sim(sys)
 {
-    var error = angle_wrap(sys.state.orientation - sys.state.setpoint);
+    var error = angle_wrap(sys.state.orientation - setpoint2orientation(sys.state.setpoint));
 
     sys.state.orientation = angle_wrap(sys.state.orientation
-		+ Math.sign(error) * sys.settings.servo_speed * sys.settings.sample_rate);
+		- Math.sign(error) * sys.settings.servo_speed * sys.settings.sample_rate);
 
     sys.state._light_noise = urand(sys.settings.light.fg_noise)
 
-    sys.state.sensor_value = integrate((a) => incident_light(sys, a)*sensitivity(sys, a),
-			 -Math.PI, Math.PI, 100) + urand(sys.settings.sensor_noise);
+    sys.state.sensor_value = (integrate((a) => incident_light(sys, a)*sensitivity(sys, a),
+			 -Math.PI, Math.PI, 100) + urand(sys.settings.sensor_noise))*sys.settings.sensor_gain;
 }
 
 var polar_samples = 180
@@ -122,7 +128,11 @@ function update_plots(sys)
     });
 
     charts.sensor_sequence.shift();
-    charts.sensor_sequence.push(sys.state.sensor_value);
+    charts.sensor_sequence.push(sys.state.sensor_value*20);
+
+    charts.servo_sequence.shift();
+    charts.servo_sequence.push(sys.state.setpoint);
+
     var N = charts.sensor_sequence.length;
     var k = charts.sample_counter;
     charts.sample_counter += 1;
@@ -130,9 +140,14 @@ function update_plots(sys)
     charts.sensor_timeseries.setOption({
 	xAxis: {data:Array.from(Array(N).keys()).map((i) => i+k)},
 	series: [{
-	    name: 'Noise',
+	    name: 'Sensor measurement',
 	    type: 'line',
 	    data: charts.sensor_sequence
+	},{
+	    name: 'Setpoint',
+	    type: 'line',
+	    data: charts.servo_sequence,
+	    yAxisIndex: 1,
 	}]
     });
 }
@@ -146,16 +161,17 @@ function setup_charts()
     var sensor_timeseries = document.createElement('div');
     container.appendChild(sensor_timeseries)
 
-    var orientation_simeseries = document.createElement('div');
-    container.appendChild(orientation_simeseries)
+    //~ var orientation_simeseries = document.createElement('div');
+    //~ container.appendChild(orientation_simeseries)
     var sensor_angles = document.createElement('div');
     container.appendChild(sensor_angles)
 
-    charts.sensor_angles =  echarts.init(sensor_angles);
-    charts.sensor_timeseries =  echarts.init(sensor_timeseries);
-    charts.orientation_simeseries =  echarts.init(orientation_simeseries);
+    charts.sensor_angles =  echarts.init(sensor_angles, 'dark');
+    charts.sensor_timeseries =  echarts.init(sensor_timeseries, 'dark');
+    //charts.orientation_simeseries =  echarts.init(orientation_simeseries,  'dark');
 
     charts.sensor_sequence = Array.from(Array(100).keys()).map((i) => 0);
+    charts.servo_sequence = Array.from(Array(100).keys()).map((i) => 0);
     charts.sample_counter = 0;
 
     charts.sensor_angles.setOption({
@@ -172,7 +188,8 @@ function setup_charts()
 	},
 	angleAxis: {
 	    type: 'value',
-	    startAngle: 0
+	    startAngle: 180,
+	    clockwise: false
 	},
 	radiusAxis: {}
     });
@@ -182,7 +199,15 @@ function setup_charts()
 	    text: 'Sensor value'
 	},
 	tooltip: {},
-	yAxis: {},
+	yAxis: [{
+		 type: 'value',
+		 name: "Sensor reading"
+		},
+		{
+		 type: 'value',
+		 name: "Servo value",
+	        }
+		],
 	xAxis: {data:Array.from(Array(25).keys())},
     });
 }
@@ -192,6 +217,13 @@ function setup_all()
     var counter = 0;
 
     setup_charts()
+
+    window.addEventListener("load", function() {
+	charts.sensor_angles.resize();
+	charts.sensor_timeseries.resize();
+    }, false);
+
+    charts.sensor_angles.resize()
 
     system._interval_id = window.setInterval(function () {
 	update_sim(system)
